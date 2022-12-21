eb73b23f058384032804a445ffe1f0716595c06c
Bestower Dev <support@bestower.org>
2022-12-21 15:33:06 -0500

    Initial Commit

---
+++ BestowerIOSApp/BestowerIOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BestowerIOSAppApp.swift
+//  BestowerIOSApp
+//
+//  Created by user232215 on 12/21/22.
+//
+
+import SwiftUI
+
+@main
+struct BestowerIOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BestowerIOSAppUITests/BestowerIOSAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BestowerIOSAppUITestsLaunchTests.swift
+//  BestowerIOSAppUITests
+//
+//  Created by user232215 on 12/21/22.
+//
+
+import XCTest
+
+final class BestowerIOSAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
