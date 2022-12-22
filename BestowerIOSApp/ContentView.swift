d8be1103bc7ef7848d096c9b4f93e65891b85266
Bestower Dev <support@bestower.org>
2022-12-21 21:54:47 -0500

    testing new authentication token

--- BestowerIOSApp/ContentView.swift
+++ BestowerIOSApp/ContentView.swift
@@ -5,23 +5,3 @@
 //  Created by user232215 on 12/21/22.
 //
 
-import SwiftUI
-
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world")
-        }
-        .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
-// this comment was added by tahsin
